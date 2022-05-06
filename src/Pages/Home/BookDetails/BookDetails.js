9a4983f454521f5d1da1e717694c686b50fbc2a1
monirhosenakanda <monirhosenakanda@gmail.com>
2022-05-06 13:03:39 +0600

    book detals data load

--- src/Pages/Home/BookDetails/BookDetails.js
+++ src/Pages/Home/BookDetails/BookDetails.js
@@ -1,12 +1,22 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 
 const BookDetails = () => {
     const {id} = useParams(); 
+    const{name} = useParams()
+    const [book,setBook] = useState({});
+    useEffect(() => {
+        const url = `http://localhost:5000/book/${id}`;
+        fetch(url)
+        .then( res => res.json())
+        .then(data => setBook(data))
+    }, []);
     console.log(id);
+    console.log(name);
     return (
         <div>
             <h1>Book Details for{id} </h1>
+            <h1>Book Details for{name} </h1>
         </div>
     );
 };
